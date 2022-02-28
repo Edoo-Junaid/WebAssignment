3acd0ad0bae2018017c661a20919a9b9ac3a6b50
Yahya Hussein <yahyamoh1504@gmail.com>
2022-02-28 11:38:09 +0400

    akkk

---
+++ database/DBController.php
@@ -0,0 +1,38 @@
+<?php
+
+class DBController
+{
+    //Database connection
+
+    protected $host= 'localhost';
+    protected $user='yahya';
+    protected $password='1234';
+    protected $database='ecommerce';
+
+    //connecion property
+    public $con=null;
+
+    //call constructor
+
+    public function __construct()
+    {
+        $this->con=mysqli_connect($this->host,$this->user,$this->password,$this->database);
+        if($this->con->connect_error){
+            echo"Fail" .this->con->connect_error;
+        }
+        echo 'connection successful..!';
+    }
+
+    public function destruct(){
+        this->closeConnection;
+
+    }
+//Closing connection
+
+protected  function closeConnection(){
+    if(this->con!=null){
+        $this->con->close();
+        $this->null;
+    }
+}
+}
---
+++ navbar.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <title>Stylish Boutique</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <?php
+    //require function.php
+    require('functions.php');
+    ?>
+  </head>
+  <body>
+
+  <div class ="navbarContainer">
+          <div class="navbar">
+         
+           <div class="logo"> <img src="Images/logo2.jpg" width="150px" height="70px"  ></div>
+          <nav>
+          <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Products</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Content</a></li>
+          <li><a href="">Account</a></li>
+          </ul>
+          </nav>
+           <div class="cart"><img src="Images/6141387_add_cart_plus_shopping_icon.png"></div>
+       </div>
+    </div>

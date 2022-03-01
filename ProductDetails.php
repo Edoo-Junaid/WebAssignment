4769e4b5f24f7c7f9cdea9f6025174ead31d96d2
Edoo-Junaid <Edooj511@gmail.com>
2022-03-01 11:55:42 +0400

    cc

--- ProductDetails.php
+++ ProductDetails.php
@@ -1,56 +1,54 @@
-
 <?php
 session_start();
 //include navbar
 include("navbar.php");
 ?>
 <?php
-$ID=$_GET['ID']??1;
-foreach($product->getData() as $item):
-if($item['ID']==$ID):
-  
+$ID = $_GET['ID'] ?? 1;
+foreach ($product->getData() as $item) :
+    if ($item['ID'] == $ID) :
+
 ?>
 
-<!-------------------single product details----------------->
-<div class="small-container1  single-product">
-    <div class="row2">
-        <div class="column5"><img src="<?php echo $item['pictures'];?>" width="90%" id="man1"></div>
-        
-        <div class="column6"><p style="font-weight: bold;"> shirt</p>
-        <h1 style="text-align: left;">shirt</h1>
-        <h4 style="font-weight: bold;"> <?php echo $item['Title']; ?></h4>
-        <form method="post" action="Process.php">
-        <select name="size">
-            <option>Select Size</option>
-            <option>XXL</option>
-            <option>XL</option>
-            <option>Large</option>
-            <option>Medium</option>
-            <option>Small</option>  
-        </select>
-        <br><br>
-        <label for="Quantity">Quantity:</label><br><br>
-        <input type="text" value="1" name="Quantity"><br>
-        <br>
-        <?php 
-             $_SESSION['productID']=$ID;
-        ?>
-         <div class="btn"><input type="submit" value="Add to cart" name="AddToCart"></div>
-       </form>
+        <!-------------------single product details----------------->
+        <div class="small-container1  single-product">
+            <div class="row2">
+                <div class="column5"><img src="<?php echo $item['pictures']; ?>" width="90%" id="man1"></div>
+
+                <div class="column6">
+                    <p style="font-weight: bold;">Item</p>
+                    <h1 style="text-align: left;"><?php echo $item['category']; ?></h1>
+                    <h4 style="font-weight: bold;"> <?php echo $item['Title']; ?></h4>
+                    <form method="post" action="Process.php">
+                        <select name="size">
+                            <option>Select Size</option>
+                            <option>XXL</option>
+                            <option>XL</option>
+                            <option>Large</option>
+                            <option>Medium</option>
+                            <option>Small</option>
+                        </select>
+                        <br>
+                        <label for="Quantity">Quantity:</label><br>
+                        <input type="text" value="1" name="Quantity"><br>
+                        <br>
+                        <?php
+                        $_SESSION['productID'] = $ID;
+                        ?>
+                        <div class="btn"><input type="submit" value="Add to cart" name="AddToCart"></div>
+                    </form>
+                </div>
+            </div>
+            <div>
+                <h3>Product Details</h3>
+                <h3><?php echo $item['Item_Details'] ?></h3>
+            </div>
         </div>
-    </div>
-           <div><h3>Product Details</h3>
-            <p> <?php echo $item['
-            
-            
-            Title'] ?></p>
-            <img src="Products/Clothing/manFashion/man1/size_table.png" width="600px" height="500px">
-         </div>
-</div>
-</body>
-</html>
+        </body>
+
+        </html>
 
 <?php
-endif;
+    endif;
 endforeach;
 ?>

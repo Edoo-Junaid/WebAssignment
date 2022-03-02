962447d1dc288742a844ef96f5f1a28b17675d83
Edoo-Junaid <Edooj511@gmail.com>
2022-03-02 22:25:08 +0400

    x

--- contactUs.php
+++ contactUs.php
@@ -7,29 +7,12 @@
 </head>
 
 <body>
-    <header>
-        <h2> STYLISH BOUTIQUE</h2>
-
-        <nav>
-            <ul class="nav_links">
-                <li><a href="main.php">Home</a></li>
-                <li><a href="AllProductPage.php">Products</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Content</a></li>
-                <li><a href="">Account</a></li>
-
-            </ul>
-        </nav>
-        <div class="Icons">
-            <img class="cart" src="images/cart.png" alt="logo">
-            <img class="user_Icon" src="images/userIcon.png">
-
-        </div>
-    </header>
+    <?php
+    include("navbar.php")
+    ?>
 
     <div class="contactUsTitle">
         <h1>HAVE SOME QUESTIONS?</h1>
-
     </div>
 
     <div class="contactPage">

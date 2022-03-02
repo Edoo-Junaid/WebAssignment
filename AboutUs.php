ff71792ca2afc18ce9f4ce2cc1fae498f33de454
Yahya Hussein <yahyamoh1504@gmail.com>
2022-03-02 09:58:29 +0400

    ss

---
+++ AboutUs.php
@@ -0,0 +1,29 @@
+<?php
+//include navbar
+include("navbar.php");
+?>
+
+<div class="products">
+    <div clas="row-container">
+
+    <h1 class="title">ABOUT US</h1>
+    
+
+
+
+
+     </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+<?php
+include("Footer.php");
+?>

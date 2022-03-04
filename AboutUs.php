24cd28bf9cad57de7103bbfac526dfadb84b6fa6
Edoo-Junaid <Edooj511@gmail.com>
2022-03-04 22:28:38 +0400

    dd

--- AboutUs.php
+++ AboutUs.php
@@ -4,16 +4,16 @@ include("navbar.php");
 ?>
 <div class="AboutUs">
     <div class="About-container">
-
-        <h1 class="title">ABOUT US</h1>
-        <h1 class="title">Who are we</h1>
-        <div class="desc">
-            <h3>We believe in a world where you have total freedom to be you, without judgement.
-                we do everything we can to help you find your fit, offering our Stylish boutik. Brands in more various sizes – and we're committed to providing all sizes at the same price – so you can be confident we’ve got the perfect thing for you. So, we make sure everyone has an equal chance to discover all the amazing things they’re capable of – no matter who they are, where they’re from or what looks they like to boss. We exist to give you the confidence to be whoever you want to be.
-                <br>We help brands and sellers connect with many shoppers on Stylish Shop. With 5 years of eCommerce expertise, we’ll be your partner in crafting effective channel, audience, and media strategies. Whether your focus is on broad awareness
-            </h3>
+        <div class="grey">
+            <h1 class="title">ABOUT US</h1>
+            <h1 class="title">Who are we</h1>
+            <div class="desc">
+                <h3>We believe in a world where you have total freedom to be you, without judgement.
+                    we do everything we can to help you find your fit, offering our Stylish boutik. Brands in more various sizes – and we're committed to providing all sizes at the same price – so you can be confident we’ve got the perfect thing for you. So, we make sure everyone has an equal chance to discover all the amazing things they’re capable of – no matter who they are, where they’re from or what looks they like to boss. We exist to give you the confidence to be whoever you want to be.
+                    <br>We help brands and sellers connect with many shoppers on Stylish Shop. With 5 years of eCommerce expertise, we’ll be your partner in crafting effective channel, audience, and media strategies. Whether your focus is on broad awareness
+                </h3>
+            </div>
         </div>
-
         <h1 class="title">Our brands</h1>
         <div class="desc">
             <h3>Get more than 20 brands straight to your pocket which include clothings , shoes ,watches , jewelries and Android phones and accessories.<h3>
@@ -49,8 +49,6 @@ include("navbar.php");
             <h3>We don’t care about where you’ve come from – we care about where you can take us. We need people who take pride in what they do and take ownership of their decisions. We need candidates who will stand by their convictions, and act decisively, commercially and instinctively in the face of challenge.
             </h3>
         </div>
-
-    
         <div class="desc">
             <h3>We're committed to providing a secure and fair marketplace for our buyers and sellers. To support this commitment, we've put in place rules and policies that govern our expectations of buyers and sellers, the actions we'll take to keep you safe, and how we'll protect you if something goes wrong.
                 The Verified Rights Owner (VeRO) program allows owners of intellectual property (IP) rights and their authorized representatives to report to us listings that may infringe on those rights. VeRO embodies our commitment to provide a safe place to buy and sell, which respects property owners' rights.

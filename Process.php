1748c4f3cce51bac75691cf772eecff1e7ee971d
Edoo-Junaid <Edooj511@gmail.com>
2022-03-04 20:05:38 +0400

    dd

--- Process.php
+++ Process.php
@@ -105,6 +105,17 @@ if (isset($_POST["ShippingDetails"])) {
     }
 }
 
+if (isset($_POST['cardDetails'])) {
+    $userID = $_SESSION['userID'];
+    $cardNumber = $_POST['cardNumber'];
+    $name = $_POST['cardName'];
+    $expDate = $_POST['expiry'];
+    $securityCode = $_POST['securityCode'];
+
+    $query = "INSERT INTO cardDetails VALUES ($userID,$cardNumber,'$name','$expDate',$securityCode)";
+    mysqli_query($connection, $query);
+}
+
 if (isset($_GET["deleteCartID"])) {
     $cartID = $_GET["deleteCartID"];
     $query = "DELETE FROM cart WHERE cart_id=$cartID";
